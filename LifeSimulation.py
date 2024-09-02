cfcd5217faebc3ba3298f08a68774be3db532116
Neel-Ch <neel.chellapilla@gmail.com>
2024-09-02 12:31:09 -0700

    Update LifeSimulation.py

--- LifeSimulation.py
+++ LifeSimulation.py
@@ -3,6 +3,8 @@ import time
 import math
 import random
 
+# create the list of cells for the board
+# cells are spawned at a frequency of 1/abundance
 def create_cells(abundance=4, size=9):
     cell_list = []
     for i in range(size):
@@ -13,6 +15,7 @@ def create_cells(abundance=4, size=9):
             cell_list.append(" ")
     return cell_list
 
+# class for game board
 class Board:
     def __init__(self, cells, width=20):
         self.cells = cells
@@ -26,6 +29,7 @@ class Board:
     def display(self):
         num_cells = len(self.cells)
         length = int(num_cells / self.width)
+        # print the cells in a width x length board
         for row in range(length):
             for column in range(self.width):
                 print(self.cells[self.width*row + column], end="")
@@ -41,12 +45,15 @@ class LifeSimulation:
         self.width = width
         self.Board = Board(self.cells, self.width)
 
+    # one generation
     def step(self, buffer):
         time.sleep(buffer)
 
         num_cells = len(self.cells)
 
         for index, cell in enumerate(self.cells):
+
+            # cells in moore cell neighborhood
             neighbor_cells = [self.cells[index - 1] if index % self.width != 0 else " ",
                               self.cells[index + 1] if index % self.width != self.width - 1 else " ",
                               self.cells[index - self.width] if index >= self.width else " ",
@@ -55,23 +62,33 @@ class LifeSimulation:
                               self.cells[index - self.width - 1] if index - 1 >= self.width else " ",
                               self.cells[index + self.width + 1] if index + 1 < num_cells - self.width else " ",
                               self.cells[index + self.width - 1] if index - 1 < num_cells - self.width else " "]
+
             dead_neighbors = 0
             for neighbor in neighbor_cells:
                 if neighbor == " ":
                     dead_neighbors += 1
+            # if there are 3 live cells in a dead cell's neighborhood, it becomes a live cell
             if self.cells[index] == " " and dead_neighbors == 5:
                 self.cells[index] = "O"
+            # only if there are 2 or 3 cells in a live cell's neighborhood does it survive, otherwise it dies
             elif self.cells[index] == "O":
                 if not dead_neighbors == 6 and not dead_neighbors == 5:
                     self.cells[index] = " "
 
 def __main__():
+    # create cells
     cells = create_cells(size=25*200, abundance=20)
+    
+    # define simulation
     sim = LifeSimulation(cells, width=200)
+    
+    # display board
     sim.Board.display()
 
-    for i in range(100): 
+    # run 100 generations with 0.5 sec buffer
+    for i in range(100):
         sim.step(0.5)
+        # clear screen and show next generation
         os.system("cls")
         sim.Board.display()
 
